c73e21e19cf407db88284d97cb58d0f6d59cb8fd
giov <giovannimaendra@gmail.com>
2025-12-06 10:38:08 +0700

    change order navbar

--- app/Views/main.php
+++ app/Views/main.php
@@ -44,8 +44,8 @@
           <li><a href="#about">Tentang Kami</a></li>
           <li><a href="#services">Layanan</a></li>
           <li><a href="#portfolio">Projects</a></li>
-          <li><a href="#contact">Kontak</a></li>
           <li><a href="#team">Tim</a></li>
+          <li><a href="#contact">Kontak</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
